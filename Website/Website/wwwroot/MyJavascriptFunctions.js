d6a40923ef802877d16264755fc88502bd739efb
Brian2524 <b2hinkle@gmail.com>
2022-03-11 23:38:25 -0500

    AnchorNavigation component to do our page navigation instead of href with hashtag. Blazor needs this because standard href with blazor doesn't work by defualt. Also had tomake a javascript file to this component

---
+++ Website/Website/wwwroot/MyJavascriptFunctions.js
@@ -0,0 +1,10 @@
+function BlazorScrollToId(id) {
+    const element = document.getElementById(id);
+if (element instanceof HTMLElement) {
+    element.scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+        inline: "nearest"
+    });
+    }
+}
