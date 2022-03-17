6b5e6d01336bfe6629716d84d356a0f27839334a
Brian2524 <b2hinkle@gmail.com>
2022-03-17 08:30:54 -0400

    Make ScrollSpy detect sections at the right time (since navbar is taking up some of the space). Idn't currently accurate when navbar is expanded. Also get rid of page content div since we arn't giving top padding to the page content anymore (navbar is now transparent

--- Website/Website/Pages/Portfolio.razor.js
+++ Website/Website/Pages/Portfolio.razor.js
@@ -1,7 +1,20 @@
-﻿const PortfolioNavbarSizeChanged = new ResizeObserver(function (entries)
+﻿var documentScrollSpy;
+var navbarHeight;
+
+const PortfolioNavbarSizeChanged = new ResizeObserver(function (entries)
 {
-    var navbarHeight = document.getElementById("PortfolioNavbar").getBoundingClientRect().height;
-    document.getElementById("PageContent").style.paddingTop = navbarHeight.toString() + "px";
+    navbarHeight = document.getElementById("PortfolioNavbar").getBoundingClientRect().height;
+
+
+    /* doesn't work for some reason. We're just going to accept ScrollSpy isn't going to be accutate when navbar is expanded
+    if ($('body').scrollspy)
+    {
+        console.log(navbarHeight);
+        $('body').scrollspy.offset = navbarHeight+1;
+        // force scrollspy to recalculate the offsets to your targets
+        $('body').scrollspy.refresh();
+        $('body').scrollspy("refresh");
+    }*/
 });
 
 
@@ -19,9 +32,32 @@ export function init()
     // Activate Bootstrap scrollspy on the main nav element
     const mainNav = document.body.querySelector('#PortfolioNavbar');
     if (mainNav) {
-        new bootstrap.ScrollSpy(document.body, {
+        documentScrollSpy = new bootstrap.ScrollSpy(document.body,
+        {
             target: '#PortfolioNavbar',
-            offset: 72,
+            offset: document.getElementById("PortfolioNavbar").getBoundingClientRect().height+1,
         });
     };
+
+
+
 }
+
+
+export function ScrollToElementWithNavbarOffset(elementId)
+{
+    const element = document.getElementById(elementId);
+    ScrollToElementWithOffset(element, navbarHeight);
+}
+function ScrollToElementWithOffset(element, offset)
+{
+    const bodyRect = document.body.getBoundingClientRect().top;
+    const elementRect = element.getBoundingClientRect().top;
+    const elementPosition = elementRect - bodyRect;
+    const offsetPosition = elementPosition - offset;
+    window.scrollTo({
+        top: offsetPosition,
+        behavior: 'smooth'
+    });
+}
+    
