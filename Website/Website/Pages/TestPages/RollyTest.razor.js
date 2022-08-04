e211da997a79ac1e94e2fdb8a4d18e89b56b2c90
Brian2524 <b2hinkle@gmail.com>
2022-08-04 13:19:11 -0500

    Finish implementing infinate parallax child divs with help of Rolly.
    - RollyTest page finished

--- Website/Website/Pages/TestPages/RollyTest.razor.js
+++ Website/Website/Pages/TestPages/RollyTest.razor.js
@@ -1,12 +1,9 @@
 ﻿export function init() {
-    /*const r = rolly({
-        view: document.querySelector('.app'),
-        native: true,
-        ease: 1 // default ease value (0 = infinately long easing, 1 = infinately fast easing)
-        // other options
-    });
-    r.init();*/
 
+    /* 
+     * This config allows for infinite child div parallaxing. For the child div use data-child="AnyChildName" instead of data-scene, and set its data-speed value.
+     * Child speeds set to 1 will match the speed if its parent.
+     */
     const config = {
         view: document.querySelector('.app'),
         ease: 1, // default ease value (0 = infinately long easing, 1 = infinately fast easing)
@@ -28,56 +25,20 @@
                 change({ globalState, sceneState, transform }) {
                     const { children } = sceneState.cache;
                     children.forEach(child => {
+                        var amt = transform * child.speed - transform;
+                        child.context.style[globalState.transformPrefix] = `translate3d(0, ${amt}px, 0)`;
 
-                        if (child.type === 'b') {
-                            child.context.style["background-color"] = "rgba(0, 0, 0, .2)";
-                            // do something specific regarding this type of child
-                        } else {
-                            // we just apply a regular transform
 
-                            // here, the speed is applied regarding the scene itself, not the view
 
-                            var amt = transform * child.speed - transform;
-                            child.context.style[globalState.transformPrefix] = `translate3d(0, ${amt}px, 0)`;
+                        if (child.type === 'b')
+                        {
+                            // Any extra specific child stuff here
                         }
                     })
                 },
             },
         },
     };
-
-/*scenes: {
-            custom: {
-                cache(cache) {
-                    const elements = document.querySelectorAll('[data-child]'); // replace cache with document maybe?
-                    const children = [];
-                    elements.forEach(element => {
-                        children.push({
-                            context: element,
-                            type: element.getAttribute('data-child'),
-                            speed: element.getAttribute('data-speed') || 1,
-                        });
-                    });
-                    return { children };
-                },
-
-                change({ globalState, sceneState, transform }) {
-                    const { children } = sceneState.cache;
-                    children.forEach(child => {
-                        if (child.type === 'b') {
-                            // do something specific regarding this type of child
-                        } else {
-                            // we just apply a regular transform
-
-                            // here, the speed is applied regarding the scene itself, not the view
-                            const transform = transform * child.speed - transform;
-
-                            child.context.style[globalState.transformPrefix] = transform;
-                        }
-                    })
-                },
-            },
-        },*/
 
     const r = window.rolly(config);
     r.init();
