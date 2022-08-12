f776b095fda4bc32c1647f93ced4f1d751615d99
Brian2524 <b2hinkle@gmail.com>
2022-08-11 23:11:44 -0500

    New library "Luxy" to replace "Rolly". This library seems to be exactly how I want parallax to be implemented as, besides some things that seem to be giving me some trouble (see NewPortfolio.js for details)

--- Website/Website/Pages/NewPortfolio.razor.js
+++ Website/Website/Pages/NewPortfolio.razor.js
@@ -2,48 +2,17 @@
 
 export function init()
 {
-    /* 
-     * This config allows for infinite child div parallaxing. For the child div use data-child="AnyChildName" instead of data-scene, and set its data-speed value.
-     * Child speeds set to 1 will match the speed if its parent.
-     */
-    const config = {
-        view: document.querySelector('.app'),
-        ease: 1, // default ease value (0 = infinately long easing, 1 = infinately fast easing)
-        scenes: {
-            custom: {
-                cache(cache) {
-                    const elements = document.querySelectorAll('[data-child]'); // replace cache with document maybe?
-                    const children = [];
-                    elements.forEach(element => {
-                        children.push({
-                            context: element,
-                            type: element.getAttribute('data-child'),
-                            speed: element.getAttribute('data-speed') || 1,
-                        });
-                    });
-                    return { children };
-                },
-
-                change({ globalState, sceneState, transform }) {
-                    const { children } = sceneState.cache;
-                    children.forEach(child => {
-                        var amt = transform * child.speed - transform;
-                        child.context.style[globalState.transformPrefix] = `translate3d(0, ${amt}px, 0)`;
-
-
-
-                        if (child.type === 'b') {
-                            // Any extra specific child stuff here
-                        }
-                    })
-                },
-            },
-        },
-    };
-
-    const r = window.rolly(config);
-    r.init();
-
-
-
+	// in the HTML, data-speed is additive, so children get parents speed and specifying another data-speed on the child element will add that speed to the accumulated parents
+	// So far this library seems almost perfect. Things i'm currently confused on....
+	//	- WTF does the speed value mean
+	//		- How do I make a super slow parallax speed (basically not moving)?
+	//		- What is the max speed? Seems to be 40 from what I can tell because after 40 things start breaking I think.
+	//	- Is scroll easing built in and not disableable? oof
+	luxy.init({
+		wrapper: '#luxy',
+		targets: '.luxy-el',
+		wrapperSpeed: 0.08,
+		targetSpeed: 0.02,
+		targetPercentage: 0.1
+	});
 }
