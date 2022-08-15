d3b7a15d5a5b6761b8abf819e1b114e78ba84435
Brian2524 <b2hinkle@gmail.com>
2022-08-15 01:37:40 -0400

    Keep "BasicScroll" library for now but we aren't using it for parallax anymore. Instead we will use the raw CSS solution for parallax.
    - Setup the parallax wrapper div
    - Make absolutly positioned background with it being the same size as the wrapper div without having to put all content inside it.
    - Havn't done any parallaxing yet, just have the setup for it now.

--- Website/Website/Pages/NewPortfolio.razor.js
+++ Website/Website/Pages/NewPortfolio.razor.js
@@ -1,49 +1,8 @@
 
-
 export function init()
 {
-	var ForegroundTranslationAmt = -1000;
-	var BackgroundTranslationAmt = 0;
-	
-	document.querySelectorAll('.Foreground').forEach((elem) =>
-	{
-		//const modifier = elem.getAttribute('data-modifier')
+	/*var PageHeight = document.querySelector('#ParallaxWrapper').offsetHeight;
+	document.querySelector('.PortfolioDottedBG').style.height = `${PageHeight}px`;
 
-		basicScroll.create(
-		{
-			elem: elem,
-			from: 0, // at scroll position of 0
-			to: 900, // stop the animation when the bottom of the element reaches the top of the viewport
-			direct: true,
-			props:
-			{
-				'--ForegroundTranslationAmt':
-				{
-					from: '0',
-					to: `${ForegroundTranslationAmt}px`
-				}
-			}
-		}).start()
-	})
-
-	document.querySelectorAll('.Background').forEach((elem) =>
-	{
-		//const modifier = elem.getAttribute('data-modifier')
-
-		basicScroll.create(
-		{
-			elem: elem,
-			from: 'top-top', // at scroll position of 0
-			to: 'bottom-bottom', // stop the animation when the bottom of the element reaches the top of the viewport
-			direct: true,
-			props:
-			{
-				'--BackgroundTranslationAmt':
-				{
-					from: '0',
-					to: `${-BackgroundTranslationAmt}px`
-				}
-			}
-		}).start()
-	})
+	console.log(`${PageHeight}px`);*/
 }
