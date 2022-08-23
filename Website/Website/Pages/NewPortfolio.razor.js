f5dd2908fde80bae02f5278aec5079a6f7a14938
Brian2524 <b2hinkle@gmail.com>
2022-08-23 17:35:43 -0400

    cleanup code and add id for background div

--- Website/Website/Pages/NewPortfolio.razor.js
+++ Website/Website/Pages/NewPortfolio.razor.js
@@ -2,7 +2,7 @@
 export function init()
 {
 	/*var PageHeight = document.querySelector('#ParallaxWrapper').offsetHeight;
-	document.querySelector('.PortfolioDottedBG').style.height = `${PageHeight}px`;
+	document.querySelector('.PageBG').style.height = `${PageHeight}px`;
 
 	console.log(`${PageHeight}px`);*/
 }
