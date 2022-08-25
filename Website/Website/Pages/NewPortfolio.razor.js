6d86235d351554356f3afac2757b83edfd605a09
Brian2524 <b2hinkle@gmail.com>
2022-08-25 13:01:41 -0400

    cleanup

--- Website/Website/Pages/NewPortfolio.razor.js
+++ Website/Website/Pages/NewPortfolio.razor.js
@@ -1,15 +1,22 @@
+var PageBGElement = document.getElementById("BG");
+var PageContentElement = document.getElementById("PageContent");
+
+
 
 export function init()
 {
 
+
 }
 
 export function UpdateBGTransform() {
-	var PageContentHeight = document.getElementById("PageContent").offsetHeight;
-	var PageBGElement = document.getElementById("BG")
+	var PageContentHeight = PageContentElement.offsetHeight;
+	
 
 	PageBGElement.style.height = `${PageContentHeight}px`;	// give BG div same height as PageContent div (PageContent basically holds everything on the page)
 	PageBGElement.style.top = `${PageContentHeight / 2}px`;	// vertically center the BG div in the center of our PageContent div
 
 	console.log(`${PageContentHeight}px`);
 }
+
+
