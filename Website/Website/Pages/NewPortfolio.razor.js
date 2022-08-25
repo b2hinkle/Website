d1f342806cdee89114562f79267627b3fa1ec8dc
Brian2524 <b2hinkle@gmail.com>
2022-08-25 15:37:19 -0400

    UpdateBGTransform on window resize and small code cleanup

--- Website/Website/Pages/NewPortfolio.razor.js
+++ Website/Website/Pages/NewPortfolio.razor.js
@@ -1,22 +1,47 @@
-var PageBGElement = document.getElementById("BG");
+var BGElement = document.getElementById("BG");
 var PageContentElement = document.getElementById("PageContent");
+var ParallaxWrapperElement = document.getElementById("ParallaxWrapper");
+
+// JQuery's ready event for when DOM is fully loaded and ready to manipulate
+$(function ()
+{
+	UpdateBGTransform();
+});
+
+// JQuery event detecting zoom/resizing of the window
+var px_ratio = window.devicePixelRatio || window.screen.availWidth / document.documentElement.clientWidth;
+$(window).resize(function ()
+{
+    var newPx_ratio = window.devicePixelRatio || window.screen.availWidth / document.documentElement.clientWidth;
+    if (newPx_ratio != px_ratio) {
+        px_ratio = newPx_ratio;
+        console.log("zooming");
+    }
+    else {
+        console.log("just resizing");
+    }
 
 
 
-export function init()
+    UpdateBGTransform(); // Keep BG div's transform updated
+});
+
+
+export function OnAfterRenderAsync()
 {
 
-
 }
 
-export function UpdateBGTransform() {
+export function UpdateBGTransform()
+{
 	var PageContentHeight = PageContentElement.offsetHeight;
-	
 
-	PageBGElement.style.height = `${PageContentHeight}px`;	// give BG div same height as PageContent div (PageContent basically holds everything on the page)
-	PageBGElement.style.top = `${PageContentHeight / 2}px`;	// vertically center the BG div in the center of our PageContent div
-
-	console.log(`${PageContentHeight}px`);
+	BGElement.style.height = `${PageContentHeight}px`;	// give BG div same height as PageContent div (PageContent basically holds everything on the page)
+	BGElement.style.top = `${PageContentHeight / 2}px`;	// vertically center the BG div in the center of our PageContent div
 }
+
+
+
+
 
 
