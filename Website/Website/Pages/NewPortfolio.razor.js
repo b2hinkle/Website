6a041bca87134fe001c95c8040e4a3aad45cbc2c
Brian2524 <b2hinkle@gmail.com>
2022-08-24 23:45:11 -0400

    Get background div to be in center of the page's content while still parallax scrolling and not extending past the page's content

--- Website/Website/Pages/NewPortfolio.razor.js
+++ Website/Website/Pages/NewPortfolio.razor.js
@@ -1,8 +1,11 @@
 
 export function init()
 {
-	/*var PageHeight = document.querySelector('#ParallaxWrapper').offsetHeight;
-	document.querySelector('.PageBG').style.height = `${PageHeight}px`;
+	var PageContentHeight = document.getElementById("PageContent").offsetHeight;
+	var PageBGElement = document.getElementById("BG")
 
-	console.log(`${PageHeight}px`);*/
+	PageBGElement.style.height = `${PageContentHeight}px`;	// give BG div same height as PageContent div (PageContent basically holds everything on the page)
+	PageBGElement.style.top = `${PageContentHeight / 2}px`;	// vertically center the BG div in the center of our PageContent div
+
+	console.log(`${PageContentHeight}px`);
 }
