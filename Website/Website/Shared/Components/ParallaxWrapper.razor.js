16709eeaad60ff909e39837c765687bf625eae33
Brian2524 <b2hinkle@gmail.com>
2022-09-13 12:21:59 -0400

    Code cleanup, improvement

--- Website/Website/Shared/Components/ParallaxWrapper.razor.js
+++ Website/Website/Shared/Components/ParallaxWrapper.razor.js
@@ -1,36 +1,41 @@
-var BGElement = document.getElementById("BG"); /* need to find a way to make this generic, so it can be any element. Maybe a list of elements? */
-var ParallaxWrapperElement = document.getElementById("ParallaxWrapper");
-var PageContentElement = document.getElementById("PageContent");
-var px_ratio = window.devicePixelRatio || window.screen.availWidth / document.documentElement.clientWidth;  // This is the zoom percentage. Can also use to convert pixel units.... (device pixel/css pixel)
-let perspectiveValue = 300;
+const BGElement = document.getElementById("BG"); /* BGElement element is currently hardcoded. Need to find a way to make this generic. Maybe a list of elements? */
+const ParallaxWrapperElement = document.getElementById("ParallaxWrapper");
+const PageContentElement = document.getElementById("PageContent");
+let px_ratio = window.devicePixelRatio || window.screen.availWidth / document.documentElement.clientWidth;  // This is the zoom percentage. Can also use to convert pixel units.... (device pixel/css pixel)
 
-/* BEGIN configurable variables */
+/* BEGIN variables */
+const perspectiveValue = 300;
 let bgZTransform = -300;
 export function SetBgZTransform(inBGZTransform)
 {
     bgZTransform = inBGZTransform;
 }
-/* END configurable variables */
+/* END variables */
 
 
 function vhToPx(inVh)
 {
     return (ParallaxWrapperElement.clientHeight / (100 / inVh));
 }
-function vwToPx(inVw) {
+function vwToPx(inVw)
+{
     return (ParallaxWrapperElement.clientWidth / (100 / inVw));
 }
 
 // JQuery event detecting zoom/resizing of the window
 $(window).resize(function ()
 {
-    var newPx_ratio = window.devicePixelRatio || window.screen.availWidth / document.documentElement.clientWidth;
-    if (newPx_ratio != px_ratio) {
+    const newPx_ratio = window.devicePixelRatio || window.screen.availWidth / document.documentElement.clientWidth;
+    if (px_ratio != newPx_ratio)
+    {
         px_ratio = newPx_ratio;
         // zooming
+
     }
-    else {
+    else
+    {
         // just resizing
+
     }
 
 
@@ -45,10 +50,10 @@ export function OnAfterRenderAsync()
      * HTML and Body elements can cause an extra scrollbar, interfiering with our parallax setup.
      * I have noticed that the HTML vertical scrolling is also used for the mobile browser's url/search bar to go away and come back. If you don't allow vertical scrolling, then the bar is always there.
      */
-    var htmlEl = document.documentElement;
+    const htmlEl = document.documentElement;
     htmlEl.style.overflowX = "hidden";
 
-    var bodyEl = document.body;
+    const bodyEl = document.body;
     bodyEl.style.margin = "0";
     bodyEl.style.overflow = "hidden";
 
@@ -58,7 +63,7 @@ export function OnAfterRenderAsync()
 
 function StartupCSSParallax()
 {
-    ParallaxWrapperElement.style.setProperty('--perspectiveValue', `${perspectiveValue}px`);
+    ParallaxWrapperElement.style.setProperty("--perspectiveValue", `${perspectiveValue}px`);
 
     UpdateParallaxBGElement();
 }
@@ -66,7 +71,7 @@ function StartupCSSParallax()
 
 function UpdateParallaxBGElement()
 {
-    BGElement.style.setProperty('--bgZTransform', `${bgZTransform}px`);
+    BGElement.style.setProperty("--bgZTransform", `${bgZTransform}px`);
     UpdateParallaxElement(BGElement, bgZTransform, false);
 }
 
@@ -83,27 +88,28 @@ function UpdateParallaxElement(inElement, inZTransform, inPreserveAspectRatio = 
     /* 
      * The formula that calculates the scale to counter the depth. "heightRatio" and "widthRatio" are variables added into the formula by me since I have the transform and perspective origin at the bottom.
      */
-    var heightScale = 1 + ((-inZTransform * heightRatio) / perspectiveValue);
-    var widthScale = 1 + ((-inZTransform * widthRatio) / perspectiveValue);
+    const heightScale = 1 + ((-inZTransform * heightRatio) / perspectiveValue);
+    const widthScale = 1 + ((-inZTransform * widthRatio) / perspectiveValue);
 
 
-    if (inPreserveAspectRatio) {
+    if (inPreserveAspectRatio)
+    {
         /*
          * If you are preserving aspect ratio, you either choose the smaller scale or the larger scale value.
          * Using smaller scale value will ensure that it stays centered with the page's content, but it won't cover whole screen if that's what you want.
          * Using larger scale value will ensure that the whole screen is covered, but then the element won't be centered with the page's content.
          * For now I'm only providing the smaller scale approach b/c I'm lazy, but larger scale one can be implemented quickly if needed.
          */
-        var smallerScaleValue = Math.min(widthScale, heightScale); // we will choose the smallest scale value, otherwise it won't be centered to the page.
+        const smallerScaleValue = Math.min(widthScale, heightScale); // we will choose the smallest scale value, otherwise it won't be centered to the page.
         // Update the css variable
-        inElement.style.setProperty('--widthScale', `${smallerScaleValue}`);
-        inElement.style.setProperty('--heightScale', `${smallerScaleValue}`);
+        inElement.style.setProperty("--widthScale", `${smallerScaleValue}`);
+        inElement.style.setProperty("--heightScale", `${smallerScaleValue}`);
         return;
     }
 
     // Update the css variable
-    inElement.style.setProperty('--widthScale', `${widthScale}`);
-    inElement.style.setProperty('--heightScale', `${heightScale}`);
+    inElement.style.setProperty("--widthScale", `${widthScale}`);
+    inElement.style.setProperty("--heightScale", `${heightScale}`);
 }
 
 
