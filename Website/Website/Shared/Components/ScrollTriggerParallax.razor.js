2473a1e53614819a05482cb4a24beb00671e1ee0
Brian2524 <b2hinkle@gmail.com>
2022-10-04 09:54:41 -0400

    Remove unnecissary scrollbars to keep page predictible

--- Website/Website/Shared/Components/ScrollTriggerParallax.razor.js
+++ Website/Website/Shared/Components/ScrollTriggerParallax.razor.js
@@ -1,5 +1,15 @@
 ﻿export function OnAfterRenderAsync()
 {
+// BEGIN CSS styling
+    /*
+     * Eliminate unnecissary scrollbars to keep page predictible
+    */
+    const htmlEl = document.documentElement;
+    htmlEl.style.overflowX = "hidden";
+    const bodyEl = document.body;
+    bodyEl.style.overflow = "hidden";
+// END CSS styling
+
     /*gsap.registerPlugin(ScrollTrigger); // still works without (maybe they made it not necissary anymore?) */ // commented out since it makes life easier not worying about who's going to register (no one will lol)
     gsap.utils.toArray(".ParallaxContainer").forEach((ParallaxContainer, i) => {
         const ParallaxElements = ImmediateChildrenQuerySelectAll(ParallaxContainer, function (elem) { return elem.matches(".ParallaxElement"); }); // get all ParallaxElements that are immediate decendents of this ParallaxContainter
