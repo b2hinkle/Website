76b403b5a378ba15122f4641a170ee615161fdd9
Brian2524 <b2hinkle@gmail.com>
2022-10-04 09:01:56 -0400

    Assign default speed if not specified and document new component

--- Website/Website/Shared/Components/ScrollTriggerParallax.razor.js
+++ Website/Website/Shared/Components/ScrollTriggerParallax.razor.js
@@ -2,11 +2,12 @@
 {
     /*gsap.registerPlugin(ScrollTrigger); // still works without (maybe they made it not necissary anymore?) */ // commented out since it makes life easier not worying about who's going to register (no one will lol)
     gsap.utils.toArray(".ParallaxContainer").forEach((ParallaxContainer, i) => {
-
         const ParallaxElements = ImmediateChildrenQuerySelectAll(ParallaxContainer, function (elem) { return elem.matches(".ParallaxElement"); }); // get all ParallaxElements that are immediate decendents of this ParallaxContainter
         ParallaxElements.forEach((ParallaxElement) =>
         {
-            const ParallaxSpeed = 1 - ParallaxElement.dataset.parallaxspeed;
+            const dataParallaxSpeed = ParallaxElement.dataset.parallaxspeed;
+            const ParallaxSpeed = dataParallaxSpeed ? 1 - dataParallaxSpeed : .5; // if not specified, give default value of .5
+
             // use function-based values in order to keep things responsive
             gsap.fromTo(ParallaxElement, {
                 y: () => `${-window.innerHeight * ParallaxSpeed}px`
