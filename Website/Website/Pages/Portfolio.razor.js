76b99bcb1b28b2a41138041ee104e732b820cbb9
Brian2524 <b2hinkle@gmail.com>
2022-10-03 20:11:48 -0500

    Seemingly working parallax without jitter

--- Website/Website/Pages/Portfolio.razor.js
+++ Website/Website/Pages/Portfolio.razor.js
@@ -1,36 +1,24 @@
 ﻿export function OnAfterRenderAsync()
 {
-    sal({
-        once: false,
-    });
-
-
     gsap.registerPlugin(ScrollTrigger);
-    /*
-    // apply parallax effect to any element with a data-speed attribute
-    gsap.to("[data-speed]", {
-        y: (i, el) => (1 - parseFloat(el.getAttribute("data-speed"))) * ScrollTrigger.maxScroll(window),
-        ease: "none",
-        scrollTrigger: {
-            start: 0,
-            end: "max",
-            invalidateOnRefresh: true,
-            scrub: 0
-        }
-    });
-    */
+
+    let getRatio = el => window.innerHeight / (window.innerHeight + el.offsetHeight);
 
 
 
-    gsap.utils.toArray(".ParallaxElement").forEach((entry, i) => {
-        // the first image (i === 0) should be handled differently because it should start at the very top.
+    gsap.utils.toArray("section").forEach((section, i) => {
+        section.ParallaxElement = section.querySelector(".ParallaxElement");
+
+
         // use function-based values in order to keep things responsive
-        gsap.fromTo(entry,
-            { y: () => i ? `${-window.innerHeight * getRatio(entry)}px` : "0px" },
-            { y: () => `${window.innerHeight * (1 - getRatio(entry))}px`, ease: "none",
-                scrollTrigger: {
-                trigger: entry,
-                start: () => i ? "top bottom" : "top top",
+        gsap.fromTo(section.ParallaxElement, {
+            y: () => `${-window.innerHeight * getRatio(section)}px`
+        }, {
+            y: () => `${window.innerHeight * (1 - getRatio(section))}px`,
+            ease: "none",
+            scrollTrigger: {
+                trigger: section,
+                start: () => "top bottom",
                 end: "bottom top",
                 scrub: true,
                 invalidateOnRefresh: true // to make it responsive
@@ -38,9 +26,4 @@
         });
 
     });
-}
-
-function getRatio(el)
-{
-    return window.innerHeight / (window.innerHeight + el.offsetHeight);
 }
