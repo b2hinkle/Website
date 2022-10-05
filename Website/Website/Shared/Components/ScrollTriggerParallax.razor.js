246c8a229166757e62344566d305e6726283faa8
Brian2524 <b2hinkle@gmail.com>
2022-10-04 23:11:47 -0400

    remove gsap

--- Website/Website/Shared/Components/ScrollTriggerParallax.razor.js
+++ Website/Website/Shared/Components/ScrollTriggerParallax.razor.js
@@ -10,28 +10,6 @@
     bodyEl.style.overflow = "hidden";
 // END CSS styling
 
-    /*gsap.registerPlugin(ScrollTrigger); // still works without (maybe they made it not necissary anymore?) */ // commented out since it makes life easier not worying about who's going to register (no one will lol)
-    gsap.utils.toArray(".ParallaxContainer").forEach((ParallaxContainer, i) => {
-        const ParallaxElements = ImmediateChildrenQuerySelectAll(ParallaxContainer, function (elem) { return elem.matches(".ParallaxElement"); }); // get all ParallaxElements that are immediate decendents of this ParallaxContainter
-        ParallaxElements.forEach((ParallaxElement) =>
-        {
-            let dataParallaxSpeed = ParallaxElement.dataset.parallaxspeed;
-            dataParallaxSpeed = dataParallaxSpeed ? dataParallaxSpeed : .5; // if not specified, give default value of .5
-            const speedMultiplier = 1 - dataParallaxSpeed;
 
-            // use function-based values in order to keep things responsive
-            gsap.fromTo(ParallaxElement, {
-                y: () => `${-window.innerHeight * speedMultiplier}px`
-            }, {
-                y: () => `${window.innerHeight * speedMultiplier}px`,
-                ease: "none",
-                scrollTrigger: {
-                    trigger: ParallaxContainer,
-                    start: () => "top bottom",
-                    end: "bottom top",
-                    scrub: true
-                }
-            });
-        });
-    });
+
 }
