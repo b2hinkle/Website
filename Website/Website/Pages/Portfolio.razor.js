c75af6b78559b2f60abb415e5d2ca0a731287ba2
Brian2524 <b2hinkle@gmail.com>
2022-10-09 02:47:05 -0400

    FINALLY GET WORKING PARALLAX SOLUTION
    - Use CSS 3D styles method in combination with a little js to work aruond the overflow:hidden not working with parallax. We are now using the clip-path property on the img tag in order to achieve an overflow:hidden effect. The reason this works is because in js we can control a css variable "--ClipBottom" dynamically by calculating where it should clip every tick().
    - Still needs to be generic, still needs better fallbacking.
    - This is first commit the result works perfect in

--- Website/Website/Pages/Portfolio.razor.js
+++ Website/Website/Pages/Portfolio.razor.js
@@ -1,6 +1,23 @@
-﻿export function OnAfterRenderAsync()
+﻿const ParallaxWrapperElement = document.getElementById("ParallaxWrapper");
+let HeaderEl = document.getElementById("Header");
+let ElToClip = document.getElementById("ElToClip");
+
+export function OnAfterRenderAsync()
 {
     sal({
         once: false,
+    });
+
+
+    let ID = window.requestAnimationFrame(function Tick(timestamp)
+    {
+        let BottomDifference = (ElToClip.getBoundingClientRect().bottom - HeaderEl.getBoundingClientRect().bottom);
+        let ClipBottom = (HeaderEl.getBoundingClientRect().bottom + ParallaxWrapperElement.scrollTop) - BottomDifference;
+        ElToClip.style.setProperty("--ClipBottom", `${ClipBottom}px`);
+
+
+
+
+        ID = requestAnimationFrame(Tick);
     });
 }
