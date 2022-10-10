b2ab9bb8a34249ff864f5e30bf77301a84d2462a
Brian2524 <b2hinkle@gmail.com>
2022-10-09 21:53:43 -0400

    Move parallax logic to the ParallaxWrapper component

--- Website/Website/Shared/Components/ParallaxWrapper.razor.js
+++ Website/Website/Shared/Components/ParallaxWrapper.razor.js
@@ -1,3 +1,6 @@
+const TestParallaxContainerEl = document.getElementById("TestParallaxContainer");
+const TestParallaxElement = document.getElementById("TestParallaxElement");
+
 export function OnAfterRenderAsync()
 {
 // BEGIN CSS styling
@@ -14,6 +17,34 @@ export function OnAfterRenderAsync()
     bodyEl.style.padding = "0px";
     bodyEl.style.margin = "0px";
 // END CSS styling
+
+
+
+
+    let ID = window.requestAnimationFrame(function Tick(timestamp)
+    {
+        let OverflowBottom = (TestParallaxElement.getBoundingClientRect().bottom - TestParallaxContainerEl.getBoundingClientRect().bottom);
+        let ClipBottom = TestParallaxElement.getBoundingClientRect().height - OverflowBottom;
+        TestParallaxElement.style.setProperty("--ClipBottom", `${ClipBottom}px`);
+
+        let OverflowTop = (TestParallaxContainerEl.getBoundingClientRect().top - TestParallaxElement.getBoundingClientRect().top);
+        let ClipTop = OverflowTop;
+        TestParallaxElement.style.setProperty("--ClipTop", `${ClipTop}px`);
+
+        let OverflowLeft = (TestParallaxContainerEl.getBoundingClientRect().left - TestParallaxElement.getBoundingClientRect().left);
+        let ClipLeft = OverflowLeft;
+        TestParallaxElement.style.setProperty("--ClipLeft", `${ClipLeft}px`);
+
+        let OverflowRight = (TestParallaxElement.getBoundingClientRect().right - TestParallaxContainerEl.getBoundingClientRect().right);
+        let ClipRight = TestParallaxElement.getBoundingClientRect().width - OverflowRight;
+        TestParallaxElement.style.setProperty("--ClipRight", `${ClipRight}px`);
+
+
+
+
+
+        ID = requestAnimationFrame(Tick);
+    });
 }
 
 
