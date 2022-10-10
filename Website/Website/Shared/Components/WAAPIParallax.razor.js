bd36b37f6e1cc65268ebf4f17244e08fc480a234
Brian2524 <b2hinkle@gmail.com>
2022-10-09 21:49:59 -0400

    Move WAAPIParallax component from its branch to develop.
    - We will soon get rid of it once we take the code that is needed from it

---
+++ Website/Website/Shared/Components/WAAPIParallax.razor.js
@@ -0,0 +1,100 @@
+﻿export function OnAfterRenderAsync()
+{
+// BEGIN CSS styling
+    /*
+     * Eliminate unnecissary scrollbars to keep page predictible
+    */
+    const htmlEl = document.documentElement;
+    htmlEl.style.overflowX = "hidden";
+    htmlEl.style.padding = "0px";
+    htmlEl.style.margin = "0px";
+    const bodyEl = document.body;
+    bodyEl.style.overflow = "hidden";
+    bodyEl.style.padding = "0px";
+    bodyEl.style.margin = "0px";
+// END CSS styling
+
+
+    const ParallaxContainerElements = document.querySelectorAll(".ParallaxContainer");
+
+    // Make our parallax containers responsible for their parallax animations
+    ParallaxContainerElements.forEach(ParallaxContainer => {
+        const ParallaxElements = ImmediateChildrenQuerySelectAll(ParallaxContainer, function (elem) { return elem.matches(".ParallaxElement"); }); // get all ParallaxElements that are immediate decendents of this ParallaxContainter
+        let ParallaxAnimations = new Array();
+        ParallaxElements.forEach((ParallaxElement) => {
+
+            let dataParallaxSpeed = ParallaxElement.dataset.parallaxspeed;
+            dataParallaxSpeed = dataParallaxSpeed ? dataParallaxSpeed : .5; // if not specified, give default value of .5
+            const speedMultiplier = 1 - dataParallaxSpeed;
+
+            const animationOptions = {
+                duration: 1,
+                iterations: Infinity,
+                direction: "normal",
+                fill: "both",
+                easing: "linear",
+            };
+            const animation = new Animation(
+                new KeyframeEffect(
+                    ParallaxElement,
+                    {
+                        transform: [`translate3d(0, ${-window.innerHeight * speedMultiplier}px, 0)`, `translate3d(0, ${window.innerHeight * speedMultiplier}px, 0)`]
+                    },
+                    animationOptions
+                )
+            );
+            ParallaxAnimations.push(animation);
+        });
+
+        ParallaxContainer.OwnedParallaxAnimations = ParallaxAnimations;
+    });
+
+    const observerOptions = {
+        root: null,
+        rootMargin: '0px 0px',
+        threshold: 0
+    };
+    const ParallaxContainerObserver = new IntersectionObserver(entries =>
+    {
+        entries.forEach((entry) =>
+        {
+            const ParallaxContainerEl = entry.target;
+            if (entry.isIntersecting == false)
+            {
+                window.cancelAnimationFrame(ParallaxContainerEl.ParallaxTickerID);
+                ParallaxContainerEl.ParallaxTickerID = undefined;
+                return;
+            }
+
+            ParallaxContainerEl.ParallaxTickerID = window.requestAnimationFrame(function Tick(timestamp)
+            {
+                const scrollPosition = (window.scrollY + window.innerHeight);               // get scroll distance to bottom of viewport.
+                const elPosition = (scrollPosition - ParallaxContainerEl.offsetTop);               // get element's position relative to bottom of viewport.
+                const durationDistance = (window.innerHeight + ParallaxContainerEl.offsetHeight);  // set desired duration.
+                const currentProgress = (elPosition / durationDistance);                    // calculate tween progresss.
+
+                ParallaxContainerEl.OwnedParallaxAnimations.forEach(ParallaxAnimation =>
+                {
+                    ParallaxAnimation.currentTime = currentProgress * 1;
+                    //ParallaxAnimation.seek(currentProgress * ParallaxAnimation.duration);
+                });
+
+                //console.log(ParallaxContainerEl.tagName);
+                ParallaxContainerEl.ParallaxTickerID = requestAnimationFrame(Tick);
+            });
+        }),
+        observerOptions
+    });
+
+    // Start observing the parallax containers
+    ParallaxContainerElements.forEach((ParallaxContainer) =>
+    {
+        ParallaxContainerObserver.observe(ParallaxContainer);
+    })
+
+
+
+
+
+    //const ParallaxElements = ImmediateChildrenQuerySelectAll(ParallaxContainer, function (elem) { return elem.matches(".ParallaxElement"); }); // get all ParallaxElements that are immediate decendents of this ParallaxContainter
+}
