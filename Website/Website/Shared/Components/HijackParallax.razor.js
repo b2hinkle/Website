a8ea548d0e79b46c7c15c708db183cbc71db6e4b
Brian2524 <b2hinkle@gmail.com>
2022-10-13 03:38:11 -0400

    Start on a new component for scroll hijacking parallaxing

---
+++ Website/Website/Shared/Components/HijackParallax.razor.js
@@ -0,0 +1,26 @@
+﻿class Parallaxer
+{
+    constructor()
+    {
+        this.wrapper = "#ParallaxWrapper";
+        this.targets = ".ParallaxElement";
+		this.wrapperSpeed = 0.08;
+		this.targetSpeed = 0.02;
+        this.targetPercentage = 0.1;
+
+
+    }
+
+
+    calcArea()
+    {
+        
+    }
+}
+
+export function OnAfterRenderAsync()
+{
+    
+    let P = new Parallaxer();
+
+}
