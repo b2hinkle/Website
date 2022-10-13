f5b4aa2a44a1c2109b373df61a2b8eae460d994a
Brian2524 <b2hinkle@gmail.com>
2022-10-13 04:48:41 -0400

    Build arrat of target elements along with their HTML attributes

--- Website/Website/Shared/Components/HijackParallax.razor.js
+++ Website/Website/Shared/Components/HijackParallax.razor.js
@@ -19,10 +19,40 @@
 
         // Gather what we know about our page
         this.Wrapper = document.getElementById(this.WrapperID);
-        this.Targets = document.querySelectorAll(TargetClass);
+        this.TargetElements = document.querySelectorAll(TargetClass);
 
         document.body.style.height = `${this.Wrapper.clientHeight}px`; // document body will determine the height/scrolling of our page
-        
+        this.WindowHeight = window.clientHeight; // ?
+        //this.attachEvent(); // something about resizing
+
+        // Give wrapper CSS styles
+        this.wrapper.style.width = '100%';
+        this.wrapper.style.position = 'fixed';
+
+        // Build our Targets array which stores the targets along with their attributes set in HTML
+        this.Targets = [];
+        this.TargetElementsLength = TargetElements.length;
+        for (let i = 0; i < this.TargetElementsLength; i++)
+        {
+            TargetEl = TargetElements[i];
+            const offset = TargetEl.getAttribute('data-offset');
+            const speedX = TargetEl.getAttribute('data-speed-x');
+            const speedY = TargetEl.getAttribute('data-speed-Y');
+            const percentage = TargetEl.getAttribute('data-percentage');
+            const horizontal = TargetEl.getAttribute('data-horizontal');
+
+            this.Targets.push(
+            {
+                elm: TargetEl,
+                offset: offset ? offset : 0,
+                horizontal: horizontal ? horizontal : 0,
+                top: 0,
+                left: 0,
+                speedX: speedX ? speedX : 1,
+                speedY: speedY ? speedY : 1,
+                percentage: percentage ? percentage : 0
+            });
+        }
 
     }
 
