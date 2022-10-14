025a318e56ed60e5542294167cc89387d119f9c6
Brian2524 <b2hinkle@gmail.com>
2022-10-14 04:10:56 -0400

    give parallax solutions better names

--- Website/Website/Shared/Components/ParallaxSolutions/Parallax.razor.js
+++ Website/Website/Shared/Components/ParallaxSolutions/Parallax.razor.js
@@ -1,80 +1,137 @@
-﻿export function OnAfterRenderAsync()
+﻿class Parallaxer
 {
-// BEGIN CSS styling
-    /*
-     * Eliminate unnecissary scrollbars to keep page predictible
-    */
-    const htmlEl = document.documentElement;
-    htmlEl.style.overflowX = "hidden";
-    htmlEl.style.padding = "0px";
-    htmlEl.style.margin = "0px";
-    const bodyEl = document.body;
-    bodyEl.style.overflow = "hidden";
-    bodyEl.style.padding = "0px";
-    bodyEl.style.margin = "0px";
-// END CSS styling
-
-
-    const ParallaxContainerElements = document.querySelectorAll(".ParallaxContainer");
-
-    // Make our parallax containers responsible for their parallax elements
-    ParallaxContainerElements.forEach(ParallaxContainer => {
-        const ParallaxElements = ImmediateChildrenQuerySelectAll(ParallaxContainer, function (elem) { return elem.matches(".ParallaxElement"); }); // get all ParallaxElements that are immediate decendents of this ParallaxContainter
-        ParallaxElements.forEach((ParallaxElement) =>
-        {
-            let dataParallaxSpeed = ParallaxElement.dataset.parallaxspeed;
-            dataParallaxSpeed = dataParallaxSpeed ? dataParallaxSpeed : .5; // if not specified, give default value of .5
-            const speedMultiplier = 1 - dataParallaxSpeed;
-            ParallaxElement.speedMultiplier = speedMultiplier;
-        });
-
-        ParallaxContainer.OwnedParallaxElements = ParallaxElements;
-    });
-
-
-
-    const observerOptions = {
-        root: null,
-        rootMargin: '0px 0px',
-        threshold: 0
-    };
-    const ParallaxContainerObserver = new IntersectionObserver(entries =>
+    constructor(inWrapperID = "ParallaxWrapper", inParallaxContainerClass = ".ParallaxContainer", inTargetClass = ".ParallaxElement", inWrapperSpeed = 1)
     {
-        entries.forEach((entry) =>
+        this.WrapperID = inWrapperID;
+        this.ParallaxContainerClass = inParallaxContainerClass;
+        this.TargetClass = inTargetClass;
+		this.WrapperSpeed = inWrapperSpeed;
+
+
+
+        if (this.IsSupported())
         {
-            const ParallaxContainerEl = entry.target;
-            if (entry.isIntersecting == false)
+            this.Init();
+        }
+    }
+
+    IsSupported() // ensures all features we use are supported
+    {
+        this.RAF = window.requestAnimationFrame
+            || window.mozRequestAnimationFrame
+            || window.oRequestAnimationFrame
+            || window.webkitRequestAnimationFrame
+            || window.msRequestAnimationFrame;
+        window.requestAnimationFrame = requestAnimationFrame; // ?
+        this.CAF = window.cancelAnimationFrame || window.mozCancelAnimationFrame;
+        this.Window = window; // store our window so we can make call to raf
+
+
+        return document.getElementById
+            && document.querySelectorAll
+            && window.cancelAnimationFrame
+            && window.requestAnimationFrame
+            && this.RAF
+            && this.CAF
+            && this.Window;
+    }
+
+    Init()
+    {
+        // BEGIN CSS styling
+            // Get rid of annoying stuff
+            const htmlEl = document.documentElement;
+            htmlEl.style.overflowX = "hidden";
+            htmlEl.style.padding = "0px";
+            htmlEl.style.margin = "0px";
+            const bodyEl = document.body;
+            bodyEl.style.overflowX = "hidden";
+            bodyEl.style.padding = "0px";
+            bodyEl.style.margin = "0px";
+        // END CSS styling
+
+        this.Wrapper = document.getElementById(this.WrapperID);
+        this.ParallaxContainers = document.querySelectorAll(this.ParallaxContainerClass);
+        this.TargetElements = document.querySelectorAll(this.TargetClass);
+        this.WapperOffset = 0; // how offset it is from the top
+        this.WrapperScrollTop = 0; // Our version of scroll top. This tells us how far we have scrolled through our page (or at least how far the content inside the wrapper was scrolled)
+        this.prevTimestamp = -1; // -1 will indicate the first paint we are ticking on
+
+
+
+        document.body.style.height = `${this.Wrapper.clientHeight}px`; // document body will determine the height/scrolling of our page
+        //this.attachEvent(); // something about resizing
+
+        // ---------- BEGIN Init things ----------
+        this.Wrapper.style.width = '100%';
+        this.Wrapper.style.position = 'fixed';
+
+        const ParallaxContainersLength = this.ParallaxContainers.length;
+        for (let i = 0; i < ParallaxContainersLength; i++)
+        {
+            const ParallaxContainer = this.ParallaxContainers[i];
+
+            const OwnedParallaxElements = ImmediateChildrenQuerySelectAll(ParallaxContainer, function (elem) { return elem.matches(".ParallaxElement"); }); // get all ParallaxElements that are immediate decendents of this ParallaxContainter
+            OwnedParallaxElements.forEach((ParallaxElement) =>
             {
-                window.cancelAnimationFrame(ParallaxContainerEl.ParallaxTickerID);
-                ParallaxContainerEl.ParallaxTickerID = undefined;
-                return;
-            }
-
-            ParallaxContainerEl.ParallaxTickerID = window.requestAnimationFrame(function Tick(timestamp)
-            {
-                const scrollPosition = (window.scrollY + window.innerHeight);                       // get scroll distance to bottom of viewport.
-                const elPosition = (scrollPosition - ParallaxContainerEl.offsetTop);                // get element's position relative to bottom of viewport.
-                const durationDistance = (window.innerHeight + ParallaxContainerEl.offsetHeight);   // set desired duration.
-                const currentProgress = (elPosition / durationDistance);                            // calculate tween progresss.
-
-                ParallaxContainerEl.OwnedParallaxElements.forEach(ParallaxElement =>
-                {
-                    const A = -window.innerHeight * ParallaxElement.speedMultiplier;
-                    const B = window.innerHeight * ParallaxElement.speedMultiplier;
-                    const amt = Lerp(A, B, currentProgress);
-                    ParallaxElement.style.transform = `translate3d(0, ${amt}px, 0)`;
-                });
-
-                //console.log(ParallaxContainerEl.tagName);
-                ParallaxContainerEl.ParallaxTickerID = requestAnimationFrame(Tick);
+                let dataParallaxSpeed = ParallaxElement.dataset.parallaxspeed;
+                dataParallaxSpeed = dataParallaxSpeed ? dataParallaxSpeed : .5; // if not specified, give default value of .5
+                const speedMultiplier = 1 - dataParallaxSpeed;
+                ParallaxElement.speedMultiplier = speedMultiplier;
             });
-        }),
-        observerOptions
-    });
 
-    // Start observing the parallax containers
-    ParallaxContainerElements.forEach((ParallaxContainer) =>
+            ParallaxContainer.OwnedParallaxElements = OwnedParallaxElements;
+        }
+        // ---------- END Init things ----------
+
+        // Now lets animate
+        this.RAF.call(this.Window, this.Tick.bind(this));
+    }
+
+    Tick(timestamp)
     {
-        ParallaxContainerObserver.observe(ParallaxContainer);
-    })
+        /*let DeltaTime = 0;
+        if (this.prevTimestamp != -1)
+        {
+            DeltaTime = Math.min(1, (timestamp - this.prevTimestamp) / 1000);
+        }*/
+
+        // Scroll the wrapper (whole page)
+        const documentScrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+        this.WapperOffset += (documentScrollTop - this.WapperOffset) * this.WrapperSpeed;
+        this.WrapperScrollTop = (Math.round(this.WapperOffset * 100) / 100);
+        this.Wrapper.style.transform = `translate3d(0, ${-this.WrapperScrollTop}px, 0)`;
+
+        // Offset the parallax elements
+        const ParallaxContainersLength = this.ParallaxContainers.length;
+        for (let i = 0; i < ParallaxContainersLength; i++)
+        {
+            const ParallaxContainer = this.ParallaxContainers[i];
+
+            const WrapperScrollTopToBotomOfViewport = (this.WrapperScrollTop + this.Window.innerHeight);                                   // get scroll distance to bottom of viewport.
+            const elPositionRelativeToBottomOfViewport = (WrapperScrollTopToBotomOfViewport - ParallaxContainer.offsetTop);    // get element's position relative to bottom of viewport.
+            const elTravelDistance = (this.Window.innerHeight + ParallaxContainer.offsetHeight);
+            const currentProgress = (elPositionRelativeToBottomOfViewport / elTravelDistance);                          // calculate tween progresss.
+
+            const OwnedParallaxElementsLength = ParallaxContainer.OwnedParallaxElements.length;
+            for (let j = 0; j < OwnedParallaxElementsLength; j++)
+            {
+                const ParallaxElement = ParallaxContainer.OwnedParallaxElements[j];
+
+                const A = -this.Window.innerHeight * ParallaxElement.speedMultiplier;
+                const B = this.Window.innerHeight * ParallaxElement.speedMultiplier;
+                const amt = Lerp(A, B, currentProgress);
+                ParallaxElement.style.transform = `translate3d(0, ${amt}px, 0)`;
+            }
+        }
+
+        /*this.prevTimestamp = timestamp;*/
+        this.RAF.call(this.Window, this.Tick.bind(this));
+    }
+
+}
+
+export function OnAfterRenderAsync()
+{
+    const p = new Parallaxer("ParallaxWrapper", ".ParallaxContainer", ".ParallaxElement");
 }
