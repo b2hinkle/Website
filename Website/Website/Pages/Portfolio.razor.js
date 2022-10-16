898aead4e6941a97582acc8b456601e714f47b37
Brian2524 <b2hinkle@gmail.com>
2022-10-16 01:13:39 -0400

    Move html and body styling out of Parallax component and into Portfolio page

--- Website/Website/Pages/Portfolio.razor.js
+++ Website/Website/Pages/Portfolio.razor.js
@@ -1,4 +1,14 @@
 ﻿export function OnAfterRenderAsync()
 {
-    
+    // BEGIN CSS styling
+        // Get rid of annoying stuff
+        const htmlEl = document.documentElement;
+        htmlEl.style.overflowX = "hidden";
+        htmlEl.style.padding = "0px";
+        htmlEl.style.margin = "0px";
+        const bodyEl = document.body;
+        bodyEl.style.overflowX = "hidden";
+        bodyEl.style.padding = "0px";
+        bodyEl.style.margin = "0px";
+    // END CSS styling
 }
--- Website/Website/Shared/Components/Animation/Parallax.razor.js
+++ Website/Website/Shared/Components/Animation/Parallax.razor.js
@@ -180,16 +180,5 @@
 
 export function OnAfterRenderAsync()
 {
-    // BEGIN CSS styling
-        // Get rid of annoying stuff
-        const htmlEl = document.documentElement;
-        htmlEl.style.overflowX = "hidden";
-        htmlEl.style.padding = "0px";
-        htmlEl.style.margin = "0px";
-        const bodyEl = document.body;
-        bodyEl.style.overflowX = "hidden";
-        bodyEl.style.padding = "0px";
-        bodyEl.style.margin = "0px";
-    // END CSS styling
     const p = new Parallaxer("ParallaxWrapper", ".ParallaxContainer", ".ParallaxElement");
 }
