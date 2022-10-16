9091f76e7ba66e8b1cbe52743fbc3fb5db953c07
Brian2524 <b2hinkle@gmail.com>
2022-10-15 21:36:39 -0400

    Finish most of the AnimateOnIntersect blazor component

--- Website/Website/Shared/Components/Animation/AnimateOnIntersection.razor.js
+++ Website/Website/Shared/Components/Animation/AnimateOnIntersection.razor.js
@@ -41,25 +41,50 @@
             // Get data attributes so we know how they want us to make the animation
             let dataDuration = el.dataset.duration;
             dataDuration = dataDuration ? dataDuration : 1; // if not specified, give default value of 1
+            const Duration = dataDuration * 1000; // conert to miliseconds
             let dataIterations = el.dataset.duration;
             dataIterations = dataIterations ? dataIterations : 1; // if not specified, give default value of 1
-
-
             animation.effect = new KeyframeEffect(
                 el,    // target
-                {},
                 {
-                    duration: dataDuration, // 1 allows us to easily scrub through the animation as if it was a percentage
+                    opacity: [0, 1],
+                    transform: ["scale(5)", "scale(1)"]
+                },
+                {
+                    duration: Duration,
                     iterations: dataIterations,
                     direction: "normal",
                     fill: "both",
                     easing: "linear",
                 }
             );
+
+            const dataOnlyObserveOnce = el.hasAttribute("data-aoi-only-observe-once");
+            if (!dataOnlyObserveOnce)
+            {
+                animation.onfinish = (event) => { this.ElementObserver.observe(el); }
+            }
             el.Animation = animation;
         });
 
+        // Now oberve for intersections and play animation when intersected
+        this.ElementObserver = new IntersectionObserver(entries =>
+        {
+            entries.forEach((entry) =>
+            {
+                const el = entry.target;
+                console.log("h");
+                if (entry.isIntersecting)
+                {
+                    this.ElementObserver.unobserve(el); // probleme is after we do this we can't know if the element left the viewport for the duration of the animation, which means that next time we are observing, we don't actually know if we should trigger the animation again or not since the user could have not even scrolled for the duration.
+                    el.Animation.play();
+                }
+            }),
+            { root: null, rootMargin: '0% 50%', threshold: 0.5 } // observer options
+        });
+        this.ElementsToAnimate.forEach((el) => { this.ElementObserver.observe(el); });   // start observing the parallax containers
     }
+
 }
 
 export function OnAfterRenderAsync()
