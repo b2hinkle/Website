c6c0a9429b6aad0282803d481fa1211d8cf8d760
Brian2524 <b2hinkle@gmail.com>
2022-10-17 13:57:08 -0400

    Move nav bar from old portfolio to new and also move scroll functionality too

--- Website/Website/Pages/Portfolio.razor.js
+++ Website/Website/Pages/Portfolio.razor.js
@@ -1,4 +1,6 @@
-﻿export function OnAfterRenderAsync()
+﻿const navbarHeight = document.getElementById("PortfolioNavbar").getBoundingClientRect().height;
+
+export function OnAfterRenderAsync()
 {
     // BEGIN CSS styling
         // Get rid of annoying stuff
@@ -11,4 +13,21 @@
         bodyEl.style.padding = "0px";
         bodyEl.style.margin = "0px";
     // END CSS styling
+}
+
+export function ScrollToElementWithNavbarOffset(elementId)
+{
+    const element = document.getElementById(elementId);
+    ScrollToElementWithOffset(element, navbarHeight);
+}
+function ScrollToElementWithOffset(element, offset)
+{
+    const bodyRect = document.body.getBoundingClientRect().top;
+    const elementRect = element.getBoundingClientRect().top;
+    const elementPosition = elementRect - bodyRect;
+    const offsetPosition = elementPosition - offset;
+    window.scrollTo({
+        top: offsetPosition,
+        behavior: 'smooth'
+    });
 }
