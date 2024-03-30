4f05896e190f4932927c467e2b1297fbd7990409
b2hinkle <b2hinkle@gmail.com>
2024-03-29 20:44:32 -0400

    Rename project `Website` to `BrianHinkle.Server` to prepare for separating functionality from website type (server vs WASM).

--- Website/BrianHinkle.Server/Pages/Error.cshtml.cs
+++ Website/BrianHinkle.Server/Pages/Error.cshtml.cs
@@ -2,7 +2,7 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Diagnostics;
 
-namespace Website.Pages
+namespace BrianHinkle.Server.Pages
 {
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     [IgnoreAntiforgeryToken]
